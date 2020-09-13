b98cac25688542b39d4a8d8a5cf6992faa225740
Emanuel Castanho <emanuelcastanho@hotmail.com>
2020-09-13 18:00:27 +0100

    Add the basics to JS file

--- Coordinate Transformation/CoordTransf.js
+++ Coordinate Transformation/CoordTransf.js
@@ -2,3 +2,34 @@
  * Coordinate Transformation
  * @author Emanuel Castanho
 */
+
+document.getElementById("geodToGeoc").checked = "checked"; // Bug: In Mozilla when updating the page the radiobutton does not return to its default position.
+document.addEventListener("change", function(){
+    if (document.getElementById("geodToGeoc").checked) {
+        document.getElementById("lambdaORx").innerHTML = "𝜆 :";
+        document.getElementById("1degORm").innerHTML = "°";
+        document.getElementById("phiORy").innerHTML = "&#966 :";
+        document.getElementById("2degORm").innerHTML = "°";
+        document.getElementById("hORz").innerHTML = "h :";
+    } else {
+        document.getElementById("lambdaORx").innerHTML = "X :";
+        document.getElementById("1degORm").innerHTML = "m";
+        document.getElementById("phiORy").innerHTML = "Y :";
+        document.getElementById("2degORm").innerHTML = "m";
+        document.getElementById("hORz").innerHTML = "Z :";
+    }
+}); 
+
+var a, e;
+
+function Solve(){
+    a = parseFloat(document.getElementById("aInput").value.replace(",","."));
+    e = parseFloat(document.getElementById("eInput").value.replace(",","."));
+
+    if ((isNaN(a) || isNaN(e)) == true) {
+        alert("Insert valid equatorial radius and eccentricity values!\n\nExample: a = 6378137 and e = 0.08181919");
+    } 
+
+    
+
+}
